fc7a3b013c168d4a6797614008140cf5008738c4
tracer-ics2017 <tracer@njuics.org>
2023-07-09 07:29:59 -0700

    >  compile 161220000 hwx Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  07:29:59 up  4:23,  1 user,  load average: 0.98, 0.66, 0.58 643f4fcf76e5505f58ac2cae7dc66d7968a43718

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -29,9 +29,9 @@ typedef struct {
    */
   struct{
     uint32_t eax;
-    uint32_t ebx;
     uint32_t ecx;
     uint32_t edx;
+    uint32_t ebx;
     uint32_t esp;
     uint32_t ebp;
     uint32_t esi;
