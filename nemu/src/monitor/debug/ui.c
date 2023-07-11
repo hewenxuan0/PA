a46c7d13efb0596587ed6a2e44191181feeabd3c
tracer-ics2017 <tracer@njuics.org>
2023-07-11 02:44:31 -0700

    >  compile 161220000 hwx Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  02:44:31 up  1:11,  1 user,  load average: 0.11, 0.05, 0.01 e325f3a2a54dd7d5a0acc72f578674f39ae1d20b

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -90,7 +90,12 @@ static int cmd_x(char *args){
 	sscanf(args,"%d%x",&num,&exprs);
 	int i;
 	for (i = 0;i < num;i ++){
-		printf("0x%8x  0x%x\n",exprs + i*4,vaddr_read(exprs + i * 4,4));
+		printf("0x%8x\t",exprs + i*4);
+		uint32_t mem=vaddr_read(exprs=i*4,4);
+		for(int j=0;j<4;j++){
+			printf("0x%02x\t",mem&0xff);
+			mem=mem>>8;
+	}
 	}
 	return 0;
 }
