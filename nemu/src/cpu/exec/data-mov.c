9d65467d649e79fcaf9c27abe11deabff86f8660
tracer-ics2017 <tracer@njuics.org>
2023-07-17 22:33:53 -0700

    >  compile 161220000 hwx Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  22:33:53 up  5:00,  1 user,  load average: 0.11, 0.16, 0.24 e42751723be0f26907e9b8ff6b9b40044fe9019

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -58,15 +58,14 @@ make_EHelper(cltd) {
 
 make_EHelper(cwtl) {
   if (decoding.is_operand_size_16) {
-    rtl_lr_w(&t0, R_AX);
-    rtl_sext(&t0, &t0, 2);
-    rtl_sari(&t0, &t0, 16);
-    rtl_sr_w(R_DX, &t0);
+    rtl_lr_b(&t0, R_AL);
+    rtl_sext(&t0, &t0, 1);
+    rtl_sr_w(R_AX, &t0);
   }
   else {
-    rtl_lr_l(&t0, R_EAX);
-    rtl_sari(&t0, &t0, 31);
-    rtl_sr_l(R_EDX, &t0);
+    rtl_lr_w(&t0, R_AX);
+    rtl_sext(&t0, &t0, 2);
+    rtl_sr_l(R_EAX, &t0);
   }
 
   print_asm(decoding.is_operand_size_16 ? "cbtw" : "cwtl");
